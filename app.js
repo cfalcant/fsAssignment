ddcd5dff54b504a97b36b7210d97f0adec7a9868
cfalcant <cfalcant@gmail.com>
2018-01-04 00:11:59 -0700

    completed

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-var fs = require('fs');
+const fs = require('fs');
+
+fs.readFile("./challenge1/info.txt", "utf8", (err, data)=>{
+  // let fs = require('fs');
+  if (err) {
+    throw err;
+  }
+  console.log(data);
+})
+
+
+let names = fs.readFileSync('./challenge2/info.txt', 'utf-8')
+names = names  + 'Nickg\n' + 'Nickd\n' + 'Jen\n';
+fs.writeFileSync('./challenge2/info.txt', names);
+
+
+
+fs.renameSync('./challenge3/binfo.txt', './challenge3/info.txt')
+
+// fs.mkdirSync()
+// let new_location = './challenge4/';
+// fs.mkdirSync(new_location[, 0o777]
+
+
+// fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
+//   if (err) throw err;
+//   console.log('File Renamed!');
+// });
