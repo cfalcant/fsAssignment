5056c43a15e2f5ddfeaf0fce1a45813eeccf82b1
cfalcant <cfalcant@gmail.com>
2018-01-04 12:01:03 -0700

    completed

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 const fs = require('fs');
 
-fs.readFile("./challenge1/info.txt", "utf8", (err, data)=>{
-  // let fs = require('fs');
-  if (err) {
-    throw err;
-  }
-  console.log(data);
-})
+// fs.readFile("./challenge1/info.txt", "utf8", (err, data)=>{
+//   // let fs = require('fs');
+//   if (err) {
+//     throw err;
+//   }
+//   console.log(data);
+// })
 
 
-let names = fs.readFileSync('./challenge2/info.txt', 'utf-8')
-names = names  + 'Nickg\n' + 'Nickd\n' + 'Jen\n';
-fs.writeFileSync('./challenge2/info.txt', names);
+// let names = fs.readFileSync('./challenge2/info.txt', 'utf-8')
+// names = names  + 'Nickg\n' + 'Nickd\n' + 'Jen\n';
+// fs.writeFileSync('./challenge2/info.txt', names);
 
 
 
-fs.renameSync('./challenge3/binfo.txt', './challenge3/info.txt')
-
-// fs.mkdirSync()
-// let new_location = './challenge4/';
-// fs.mkdirSync(new_location[, 0o777]
+// fs.renameSync('./challenge3/binfo.txt', './challenge3/info.txt')
 
 
-// fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
-//   if (err) throw err;
-//   console.log('File Renamed!');
-// });
+// fs.mkdirSync('./challenge4/copyfolder');
+// fs.copyFileSync('./challenge4/info.txt', './challenge4/copyfolder/info.txt', 0);
